5b60d9f7fb81b755e3273122e491b80a19bd80fd
shu12388y <spl.sp999@gmail.com>
2026-03-22 17:44:34 +0530

    added streams

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { worker } from "./worker/worker.js";
 import { delete_file } from "./utils/delete_file.utils.js";
+import { createWriteStream } from "node:fs";
+import { pipeline } from "stream/promises";
+import { statusMonitor } from "hono-status-monitor";
+
 
 const _dirname = fileURLToPath(import.meta.url);
 const _filename = path.join(_dirname, "../", "upload");
@@ -20,6 +24,12 @@ is_preprocess_file_exists();
 is_hls_file_exists();
 
 const app = new Hono();
+const monitor = statusMonitor();
+
+
+app.use("*", monitor.middleware);
+
+app.route("/status", monitor.routes);
 
 app.get("/health", (c) => {
   c.status(200);
@@ -32,28 +42,27 @@ app.post("/upload", async (c) => {
     const file_name = data["video"] as File;
     const file_type = file_name.name.split(".")[1];
     const file_names = file_name.name.split(".")[0];
-    const file_uploader_worker = await worker(
-      `${_file_upload_path}/file_upload_thread.js`,
-      {
-        file_name,
-        _filename,
-        name: file_name.name,
-      },
-    );
-    if (file_uploader_worker && file_type !== "mp4") {
+    const writeStream = createWriteStream(`${_filename}/${file_name.name}`);
+    await pipeline(file_name.stream(), writeStream);
+    if (file_type !== "mp4") {
       await worker(`${_file_upload_path}/video_process_thread.js`, {
         _filename,
         name: file_name.name,
         filename: file_names,
       });
-
       await worker(`${_file_upload_path}/hls_process_thread.js`, {
         file_names: file_names,
         video_path: `${_video_path}/${file_names}.mp4`,
       });
-      await delete_file(`${_filename}/${file_name.name}`);
       await delete_file(`${_video_path}/${file_names}.mp4`);
     }
+
+    await worker(`${_file_upload_path}/hls_process_thread.js`, {
+      file_names: file_names,
+      video_path: `${_filename}/${file_names}.mp4`,
+    });
+
+    await delete_file(`${_filename}/${file_name.name}`);
 
     return c.json({ message: "success" });
   } catch (error) {
@@ -62,6 +71,7 @@ app.post("/upload", async (c) => {
 });
 
 serve({
-  fetch: app.fetch, 
+  fetch: app.fetch,
   port: 3000,
 });
+monitor.initSocket(serve);
